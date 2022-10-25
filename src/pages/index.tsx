a43838cf9a0c9a7f406b519023e91a2b26675bc3
MP <markpaololibunao@gmail.com>
2022-10-25 18:43:00 +0800

    Comments out removed procedure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
-import { trpc } from '@/utils/trpc'
+//import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-	const { data, error, isLoading } = trpc.useQuery(['hello'])
+	//const { data, error, isLoading } = trpc.useQuery(['hello'])
 
-	if (isLoading) return <p>Loading...</p>
-	if (error) return <div>{JSON.stringify(error)}</div>
+	//if (isLoading) return <p>Loading...</p>
+	//if (error) return <div>{JSON.stringify(error)}</div>
 
-	return <div>{JSON.stringify(data)}</div>
+	return <div>hello</div>
+	//return <div>{JSON.stringify(data)}</div>
 }
 
 export default Home
