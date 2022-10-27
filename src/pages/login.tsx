6450e126c725a5820fa93eaa505093c2a90725b7
MP <markpaololibunao@gmail.com>
2022-10-27 17:57:04 +0800

    Gets otp from the url to be verified later

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,16 @@
 import { CreateUserInput } from '@/server/modules/accounts/accounts.schema'
 import { trpc } from '@/utils/trpc'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 
+function VerifyToken() {
+	return <p>Verifying...</p>
+}
+
 export default function LoginPage(): JSX.Element {
+	const router = useRouter()
 	const [success, setSuccess] = useState<boolean>(false)
 
 	const { handleSubmit, register } = useForm<CreateUserInput>()
@@ -17,6 +23,12 @@ export default function LoginPage(): JSX.Element {
 
 	const onSubmit = (values: CreateUserInput) => {
 		mutate(values)
+	}
+
+	const hash = router.asPath.split('#token')[1]
+
+	if (hash) {
+		return <VerifyToken />
 	}
 
 	return (
