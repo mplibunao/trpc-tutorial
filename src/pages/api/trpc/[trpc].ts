758b9c2b80eecb0dc3cacc5655bc83d7638bd4d9
MP <markpaololibunao@gmail.com>
2022-10-27 17:47:34 +0800

    Improves error handling on trpc by also logging the path

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -5,11 +5,11 @@ import * as trpcNext from '@trpc/server/adapters/next'
 export default trpcNext.createNextApiHandler({
 	router: appRouter,
 	createContext,
-	onError: ({ error }) => {
+	onError: ({ error, path }) => {
 		if (error.code === 'INTERNAL_SERVER_ERROR') {
-			console.error('Something went wrong', error) // eslint-disable-line no-console
+			console.error(`Something went wrong on ${path}: ${error}`) // eslint-disable-line no-console
 		} else {
-			console.log('error', error) // eslint-disable-line no-console
+			console.log('error', path, error) // eslint-disable-line no-console
 		}
 	},
 })
