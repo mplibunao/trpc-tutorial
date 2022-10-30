0d5620690fee9c959f550b756dbaa59ace458c30
MP <markpaololibunao@gmail.com>
2022-10-30 11:06:28 +0800

    - Moves LoginForm to a separate component so we can force load it client-side   and fix the issue with the otp link split logic because of the url discrepancies between client/server - Fixes typo with otp link split logic that excludes the `=` from the   token (happens because we're using `#token=` instead of `?token=`)

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,64 @@
+import { CreateUserInput } from '@/server/modules/accounts/accounts.schema'
+import { trpc } from '@/utils/trpc'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+function VerifyToken({ hash }: { hash: string }) {
+	const router = useRouter()
+	const { data, isLoading } = trpc.useQuery(['users.verifyOtp', { hash }])
+
+	if (isLoading) {
+		return <p>Verifying...</p>
+	}
+
+	router.push(data?.redirect?.includes('login') ? '/' : data?.redirect || '/')
+
+	return <p>Redirecting...</p>
+}
+
+export default function LoginForm(): JSX.Element {
+	const router = useRouter()
+	const [success, setSuccess] = useState<boolean>(false)
+
+	const { handleSubmit, register } = useForm<CreateUserInput>()
+
+	const { mutate, error } = trpc.useMutation(['users.requestOtp'], {
+		onSuccess: () => {
+			setSuccess(true)
+		},
+	})
+
+	const onSubmit = (values: CreateUserInput) => {
+		mutate(values)
+	}
+
+	const hash = router.asPath.split('#token=')[1]
+
+	if (hash) {
+		return <VerifyToken hash={hash} />
+	}
+
+	return (
+		<>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				{error && error.message}
+				{success && <p>Check your email</p>}
+
+				<h1>Login</h1>
+				<input
+					type='email'
+					placeholder='jane.doe@example.com'
+					{...register('email')}
+				/>
+				<br />
+				<button type='submit'>Login</button>
+			</form>
+
+			<Link href='/register'>
+				<a>Register</a>
+			</Link>
+		</>
+	)
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,64 +1,18 @@
-import { CreateUserInput } from '@/server/modules/accounts/accounts.schema'
-import { trpc } from '@/utils/trpc'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import dynamic from 'next/dynamic'
 
-function VerifyToken({ hash }: { hash: string }) {
-	const router = useRouter()
-	const { data, isLoading } = trpc.useQuery(['users.verifyOtp', { hash }])
-
-	if (isLoading) {
-		return <p>Verifying...</p>
-	}
-
-	router.push(data?.redirect?.includes('login') ? '/' : data?.redirect || '/')
-
-	return <p>Redirecting...</p>
-}
+/*
+ *Problem is passing the token as `#token` instead of normal query params `?token` causes inconsistencies between server and client.
+ *router.asPath results in `/login` on server and `/login#token=...` on client.
+ *Changing to `?token` fixes this or just use dynamic import and set ssr: false
+ */
+const LoginForm = dynamic(() => import('@/components/LoginForm'), {
+	ssr: false,
+})
 
 export default function LoginPage(): JSX.Element {
-	const router = useRouter()
-	const [success, setSuccess] = useState<boolean>(false)
-
-	const { handleSubmit, register } = useForm<CreateUserInput>()
-
-	const { mutate, error } = trpc.useMutation(['users.requestOtp'], {
-		onSuccess: () => {
-			setSuccess(true)
-		},
-	})
-
-	const onSubmit = (values: CreateUserInput) => {
-		mutate(values)
-	}
-
-	const hash = router.asPath.split('#token')[1]
-
-	if (hash) {
-		return <VerifyToken hash={hash} />
-	}
-
 	return (
-		<>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				{error && error.message}
-				{success && <p>Check your email</p>}
-
-				<h1>Login</h1>
-				<input
-					type='email'
-					placeholder='jane.doe@example.com'
-					{...register('email')}
-				/>
-				<br />
-				<button type='submit'>Login</button>
-			</form>
-
-			<Link href='/register'>
-				<a>Register</a>
-			</Link>
-		</>
+		<div>
+			<LoginForm />
+		</div>
 	)
 }
