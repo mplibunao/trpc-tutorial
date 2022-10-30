7bcc49ff4aec5f6408148e5c6c31e625a8ce6c00
MP <markpaololibunao@gmail.com>
2022-10-30 18:16:17 +0800

    Renames procedure to posts.getPosts and posts.getPost

--- src/server/trpc/post.router.ts
+++ src/server/trpc/post.router.ts
@@ -2,5 +2,5 @@ import { createRouter } from './context'
 
 export const postRouter = createRouter()
 	.mutation('createPost', {})
-	.query('posts', {})
-	.query('post', {})
+	.query('getPosts', {})
+	.query('getPost', {})
