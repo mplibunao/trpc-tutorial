590f867d8a00d4da01783e3c9a631928409841b4
MP <markpaololibunao@gmail.com>
2022-10-30 16:53:15 +0800

    Cleans up index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
-//import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-	//const { data, error, isLoading } = trpc.useQuery(['hello'])
-
-	//if (isLoading) return <p>Loading...</p>
-	//if (error) return <div>{JSON.stringify(error)}</div>
-
 	return <div>hello</div>
-	//return <div>{JSON.stringify(data)}</div>
 }
 
 export default Home
